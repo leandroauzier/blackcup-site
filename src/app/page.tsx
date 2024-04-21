f150b623a663414f37d11b09c3cd239a1d2344f2
leandroauzier <leandroauzier02@gmail.com>
2024-04-20 23:35:06 -0300

    homepage não terminada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
-import Image from "next/image";
 import { Typography } from "@mui/material";
-import { Roboto }from '@fontsource/roboto/500.css';
-import Footer from "@/components/footer";
-import navbar from "../components/navbar";
+import Footer from "@/components/layout/footer";
+import Navbar from "@/components/layout/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <div className="justify-center text-white bg-white dark:bg-gray-950 h-screen">
-        <navbar />
+      <div className="flex flex-col justify-center text-white bg-white dark:bg-gray-950 h-screen">
+        <div className="pl-20">
+        <Navbar />
         <Typography
           variant="h1"
           color="purple"
-          className="text-8xl text-center"
+          className="text-8xl text-start"
           fontFamily="Roboto"
         >
           Blackcup
@@ -20,13 +20,21 @@ export default function Home() {
         <Typography
           variant="subtitle1"
           color="white"
-          className="text-4xl justify-center text-center"
+          className="text-4xl pl-2 justify-start text-left"
           fontFamily="Roboto"
         >
           Entertainment
         </Typography>
-        <Footer />
+        </div>
+        <Image
+          src="/images/empresa-games.jpg"
+          alt="Escola"
+          width={600}
+          height={600}
+          className="absolute inset-0 ml-auto w-[600px] h-[600px] rounded-bl-[100px] object-cover object-center"
+        />
       </div>
+      <Footer />
     </>
   );
 }
